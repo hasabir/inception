550bced1cde712836699cee702760719c50a094a
Hajar Sabir <96284181+hasabir@users.noreply.github.com>
2023-09-12 12:32:28 +0000

    update mariadb and wordpress

--- srcs/requirements/mariadb/tools/test_script.sh
+++ srcs/requirements/mariadb/tools/test_script.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-/etc/init.d/mariadb start
+service mariadb start
 
-exec mariadb
+sleep 10
+
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\`;"
+
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USERNAME}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+
+# mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_DATABASE}\`.* TO \`${DB_USERNAME}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+
+# mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+
+mariadb -e "FLUSH PRIVILEGES;"
+
+# service mariadb 
+
+# mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
+
+
+# exec mysqld_safe #run in the foreground
+
+exec mariadb #run in the background
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,49 +1,5 @@
-#!/bin/bash
-
-    1  apt install php-fpm
-    2  apt install php-mysql
-    3  systemctl status php7.4-fpm
-    4  service php7.4-fpm status
-    5  service php7.4-fpm 
-    6  service php7.4-fpm start
-    7  service php7.4-fpm status
-    8  apt install mariadb-cilent
-    9  apt install mariadb-client
-   10  apt install mariadb-server
-   11  service mariadb status
-   12  service mariadb start
-   13  service mariadb status
-   14  service php7.4-fpm 
-   15  mariad
-   16  mariadb
-   17  mariadb_secure_installation
-   18  mariadb
-   19  wget -O /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
-   20  history
-   21  apt install wget
-   22  wget -O /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
-   23  chown -R www-data.www-data /var/www/html/wordpress
-   24  tar -xzvf /tmp/wordpress.tar.gz -C /var/www/html
-   25  tar -xzvf /tmp/wordpress.tar.gz -C .
-   26  ls
-   27  chown -R www-data.www-data wordpress
-   28  ls -la wordpress
-   29  ls -a wordpress
-   30  ls -l wordpress
-   31  ls -k wordpress
-   32  ls -la
-   33  chmod -R 755 wordpress
-   34  ls -la
-   35  apt install nginx
-   36  nginx start
-   37  service nginx start
-   38  nginx
-   39  /usr/sbin/nginx
-   40  touch /etc/nginx/conf.d/wordpress.conf
-   41  vim /etc/nginx/conf.d/wordpress.conf
-   42  apt install vim
-   43  vim /etc/nginx/conf.d/wordpress.conf
-   44  rm /etc/nginx/sites-enabled/default
-   45  rm /etc/nginx/sites-available/default
-   60  nginx -t
-service nginx reload
+CREATE DATABASE wordpress_db;
+CREATE USER 'hasabir'@'localhost' IDENTIFIED BY '123';
+CREATE USER 'hasabir'@'%' IDENTIFIED BY '123';
+GRANT ALL PRIVILEGES ON wordpress_db.* to hasabir@'%';
+FLUSH PRIVILEGES;
